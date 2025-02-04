cbe8e059f166fe9fc526ac9666795e2acb9c875d
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-04 14:42:37 +0100

    set folder for static assets

--- app.js
+++ app.js
@@ -7,35 +7,39 @@ app.get('/', (req, res) => {
 })
 
 
+// definiamo l'uso di una cartella per i file statici
+app.use(express.static('public'));
+
+
 app.get('/bacheca', (req, res) => {
 
     const posts = [
         {
-            title: "Viaggio in Colombia",
+            title: "BIT ROT PROJECT 1",
             content: "Un racconto fotografico del mio viaggio attraverso la Colombia, esplorando le sue città e paesaggi unici.",
             image: "colombia-trip.jpg",
             tags: ["viaggi", "fotografia", "Colombia", "avventura"]
         },
         {
-            title: "Minimalismo nel Web Design",
+            title: "BIT ROT PROJECT 2",
             content: "L'importanza del design minimalista per migliorare l'esperienza utente e la leggibilità dei contenuti.",
             image: "minimal-webdesign.jpg",
             tags: ["web design", "UX", "minimalismo"]
         },
         {
-            title: "Fotogiornalismo: Etica e Responsabilità",
+            title: "BIT ROT PROJECT 3",
             content: "Una riflessione sull'etica del fotogiornalismo e sull'importanza di raccontare storie in modo autentico.",
             image: "photojournalism-ethics.jpg",
             tags: ["fotogiornalismo", "etica", "documentario"]
         },
         {
-            title: "Introduzione a JavaScript per Creativi",
+            title: "BIT ROT PROJECT 4",
             content: "Come JavaScript può essere usato per creare progetti interattivi nel campo delle arti visive e digitali.",
             image: "js-for-creatives.jpg",
             tags: ["JavaScript", "coding", "arte", "creatività"]
         },
         {
-            title: "Il Potere del Bianco e Nero nella Fotografia",
+            title: "BIT ROT PROJECT 5",
             content: "Perché le immagini in bianco e nero hanno ancora un forte impatto emotivo e narrativo.",
             image: "blackwhite-photography.jpg",
             tags: ["fotografia", "bianco e nero", "narrazione"]
